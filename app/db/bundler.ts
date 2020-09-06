77bb597af46d6bbba92daf98185f95262c7e196f
Faniry <faniry6@gmail.com>
2020-09-06 15:57:25 +0200

    Add Lyricist

--- app/db/bundler.ts
+++ app/db/bundler.ts
@@ -1,120 +1,164 @@
-import realm, { Artist, Song } from "."
-import { JsonDecoder } from "@artutra/ts-data-json"
-import { Playlist } from "./Playlist"
+import realm, {Artist, Song} from '.';
+import {JsonDecoder} from '@artutra/ts-data-json';
+import {Playlist} from './Playlist';
 
 export interface SongBundle {
-  id: string
-  title: string
-  content: string
-  artist: string
-  transposeAmount?: number | null | undefined
-  fontSize?: number | null | undefined
-  showTablature: boolean
-  updated_at: string
+  id: string;
+  title: string;
+  content: string;
+  artist: string;
+  transposeAmount?: number | null | undefined;
+  fontSize?: number | null | undefined;
+  showTablature: boolean;
+  updated_at: string;
+  lyricist: string;
 }
 export interface PlaylistBundle {
-  id: string
-  name: string
-  songs: { id: string }[]
+  id: string;
+  name: string;
+  songs: {id: string}[];
 }
 export interface DatabaseBundle {
-  version: number
-  created_at: string
-  songs: SongBundle[]
-  playlists: PlaylistBundle[]
+  version: number;
+  created_at: string;
+  songs: SongBundle[];
+  playlists: PlaylistBundle[];
 }
 
-export function createBundle(playlistIds?: string[], songIds?: string[]): DatabaseBundle {
-  let db: DatabaseBundle = { version: 1, songs: [], playlists: [], created_at: new Date().toJSON() }
+export function createBundle(
+  playlistIds?: string[],
+  songIds?: string[],
+): DatabaseBundle {
+  let db: DatabaseBundle = {
+    version: 1,
+    songs: [],
+    playlists: [],
+    created_at: new Date().toJSON(),
+  };
   if (songIds) {
     songIds.forEach(songId => {
-      let s = Song.getById(songId)
-      if (!s) throw new Error('Invalid song ids')
-      db.songs.push(Song.toBundle(s))
-    })
+      let s = Song.getById(songId);
+      if (!s) throw new Error('Invalid song ids');
+      db.songs.push(Song.toBundle(s));
+    });
   } else {
     realm.objects<Song>('Song').forEach(s => {
-      db.songs.push(Song.toBundle(s))
-    })
+      db.songs.push(Song.toBundle(s));
+    });
   }
   if (playlistIds) {
     playlistIds.forEach(playlistId => {
-      let p = Playlist.getById(playlistId)
-      if (!p) throw new Error('Invalid playlist id')
+      let p = Playlist.getById(playlistId);
+      if (!p) throw new Error('Invalid playlist id');
 
       for (var s in p.songs) {
-        let song = p.songs[s]
+        let song = p.songs[s];
         if (db.songs.find(s => s.id === song.id) == null) {
-          db.songs.push(Song.toBundle(song))
+          db.songs.push(Song.toBundle(song));
         }
       }
-      db.playlists.push(Playlist.toBundle(p))
-    })
+      db.playlists.push(Playlist.toBundle(p));
+    });
   } else {
     realm.objects<Playlist>('Playlist').forEach(p => {
-      db.playlists.push(Playlist.toBundle(p))
-    })
+      db.playlists.push(Playlist.toBundle(p));
+    });
   }
   return db;
 }
 export function importBundle(bundle: DatabaseBundle): void {
-  let mapExistingSongs: { [key: string]: string } = {}
+  let mapExistingSongs: {[key: string]: string} = {};
   bundle.songs.forEach(bundleSong => {
-    let artistDb: Artist | undefined = Artist.getByName(bundleSong.artist)
+    let artistDb: Artist | undefined = Artist.getByName(bundleSong.artist);
     if (artistDb == null) {
-      artistDb = Artist.create(bundleSong.artist)
+      artistDb = Artist.create(bundleSong.artist);
     }
-    let songDb = Song.getById(bundleSong.id)
+    let songDb = Song.getById(bundleSong.id);
     if (songDb) {
-      mapExistingSongs[bundleSong.id] = songDb.id
+      mapExistingSongs[bundleSong.id] = songDb.id;
       if (songDb.updated_at < new Date(bundleSong.updated_at)) {
-        Song.update(songDb.id, artistDb, bundleSong.title, bundleSong.content)
+        Song.update(
+          songDb.id,
+          artistDb,
+          songDb.lyricist,
+          bundleSong.title,
+          bundleSong.content,
+        );
       }
     } else {
-      let s = Song.create(artistDb, bundleSong.title, bundleSong.content, bundleSong.id)
-      mapExistingSongs[bundleSong.id] = s.id
+      let s = Song.create(
+        artistDb,
+        bundleSong.title,
+        bundleSong.lyricist,
+        bundleSong.content,
+        bundleSong.id,
+      );
+      mapExistingSongs[bundleSong.id] = s.id;
     }
-  })
+  });
   bundle.playlists.forEach(bundlePlaylist => {
-    let playlistDb: Playlist | undefined = Playlist.getByName(bundlePlaylist.name)
+    let playlistDb: Playlist | undefined = Playlist.getByName(
+      bundlePlaylist.name,
+    );
     if (playlistDb == null) {
-      playlistDb = Playlist.create(bundlePlaylist.name)
+      playlistDb = Playlist.create(bundlePlaylist.name);
     }
     bundlePlaylist.songs.forEach(bundleSong => {
-      let songDb = Song.getById(mapExistingSongs[bundleSong.id])
+      let songDb = Song.getById(mapExistingSongs[bundleSong.id]);
       if (songDb == null) {
-        throw new Error('Playlist song not found')
+        throw new Error('Playlist song not found');
       }
-      Playlist.addSong(playlistDb!, songDb)
-    })
-  })
+      Playlist.addSong(playlistDb!, songDb);
+    });
+  });
 }
-const bundleDecoder = JsonDecoder.object<DatabaseBundle>({
-  version: JsonDecoder.number,
-  created_at: JsonDecoder.string,
-  songs: JsonDecoder.array<SongBundle>(
-    JsonDecoder.object<SongBundle>({
-      id: JsonDecoder.string,
-      title: JsonDecoder.string,
-      content: JsonDecoder.string,
-      artist: JsonDecoder.string,
-      transposeAmount: JsonDecoder.optional(JsonDecoder.number),
-      fontSize: JsonDecoder.optional(JsonDecoder.number),
-      showTablature: JsonDecoder.boolean,
-      updated_at: JsonDecoder.string
-    }, 'SongBundle'), 'songs[]'),
-  playlists: JsonDecoder.array<PlaylistBundle>(
-    JsonDecoder.object<PlaylistBundle>({
-      id: JsonDecoder.string,
-      name: JsonDecoder.string,
-      songs: JsonDecoder.array<{ id: string }>(JsonDecoder.object<{ id: string }>({
-        id: JsonDecoder.string
-      }, 'playlist.songs.id'), 'playlist.songs[]')
-    }, 'PlaylistBundle'), 'playlists[]')
-},
-  'DatabaseBundle')
+const bundleDecoder = JsonDecoder.object<DatabaseBundle>(
+  {
+    version: JsonDecoder.number,
+    created_at: JsonDecoder.string,
+    songs: JsonDecoder.array<SongBundle>(
+      JsonDecoder.object<SongBundle>(
+        {
+          id: JsonDecoder.string,
+          title: JsonDecoder.string,
+          content: JsonDecoder.string,
+          artist: JsonDecoder.string,
+          transposeAmount: JsonDecoder.optional(JsonDecoder.number),
+          fontSize: JsonDecoder.optional(JsonDecoder.number),
+          showTablature: JsonDecoder.boolean,
+          updated_at: JsonDecoder.string,
+          lyricist: JsonDecoder.string,
+        },
+        'SongBundle',
+      ),
+      'songs[]',
+    ),
+    playlists: JsonDecoder.array<PlaylistBundle>(
+      JsonDecoder.object<PlaylistBundle>(
+        {
+          id: JsonDecoder.string,
+          name: JsonDecoder.string,
+          songs: JsonDecoder.array<{id: string}>(
+            JsonDecoder.object<{id: string}>(
+              {
+                id: JsonDecoder.string,
+              },
+              'playlist.songs.id',
+            ),
+            'playlist.songs[]',
+          ),
+        },
+        'PlaylistBundle',
+      ),
+      'playlists[]',
+    ),
+  },
+  'DatabaseBundle',
+);
 
-export async function decodeJsonBundle(jsonString: string): Promise<DatabaseBundle> {
-  let bundle = JSON.parse(jsonString)
-  return await bundleDecoder.decodePromise<DatabaseBundle>(bundle)
+export async function decodeJsonBundle(
+  jsonString: string,
+): Promise<DatabaseBundle> {
+  let bundle = JSON.parse(jsonString);
+  return await bundleDecoder.decodePromise<DatabaseBundle>(bundle);
 }
