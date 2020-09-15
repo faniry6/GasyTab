363e65c5d64783f75778ab2b98812dc8b9e8b712
Faniry <faniry6@gmail.com>
2020-09-15 11:45:45 +0200

    Change service to GasyTab

--- app/services/BaseService.ts
+++ app/services/BaseService.ts
@@ -1,22 +1,24 @@
 export interface ArtistDoc {
-  name: string
-  path: string
-  type: 'artist'
+  name: string;
+  path: string;
+  type: 'artist';
 }
 export interface SongDoc {
-  title: string
-  artist: string
-  path: string
-  type: 'song'
+  title: string;
+  artist: string;
+  lyricist: string;
+  chordSheet: string;
+  path: string;
+  type: 'song';
 }
-export type Doc = ArtistDoc | SongDoc
+export type Doc = ArtistDoc | SongDoc;
 
 export abstract class BaseService {
-  name!: string
-  baseUrl!: string
-  searchUrl!: string
-  constructor() { }
-  abstract async getSearch(query: string): Promise<Doc[]>
-  abstract async getArtistSongs(path: string): Promise<SongDoc[]>
-  abstract async getChordProSong(path: string): Promise<string>
+  name!: string;
+  baseUrl!: string;
+  searchUrl!: string;
+  constructor() {}
+  abstract async getSearch(query: string): Promise<Doc[]>;
+  abstract async getArtistSongs(path: string): Promise<SongDoc[]>;
+  abstract async getChordProSong(path: string): Promise<string>;
 }
