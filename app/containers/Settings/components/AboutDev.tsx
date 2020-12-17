e8e39092fd6e51fe395413ad0d602473dfdc6c3f
Faniry <faniry6@gmail.com>
2020-12-17 11:59:53 +0100

    Change about dev

--- app/containers/Settings/components/AboutDev.tsx
+++ app/containers/Settings/components/AboutDev.tsx
@@ -3,34 +3,25 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import {View, Text, TouchableOpacity, Linking, StyleSheet} from 'react-native';
 import LanguageContext from '../../../languages/LanguageContext';
 
-const version = '0.5.4';
+const version = '0.5.5';
 const AboutDev = () => {
   const {t} = useContext(LanguageContext);
 
   async function goToWebURL() {
-    try {
-      await Linking.openURL('https://www.gasytablature.com');
-    } catch (e) {
-      console.warn(e);
-    }
-  }
-  async function goToDevURL() {
     try {
       await Linking.openURL('https://github.com/faniry6/GasyTab');
     } catch (e) {
       console.warn(e);
     }
   }
+
   return (
     <View style={styles.container}>
       <Text style={styles.lightGray}>{t('developed_by')} </Text>
       <TouchableOpacity onPress={goToWebURL} style={styles.devButton}>
         <Text style={styles.primaryColor}>{'Ra Faniry'}</Text>
       </TouchableOpacity>
-      <Text style={styles.lightGray}>{', v.' + version + ', fork us at '}</Text>
-      <TouchableOpacity onPress={goToDevURL} style={styles.devButton}>
-        <Text style={styles.primaryColor}>GitHub</Text>
-      </TouchableOpacity>
+      <Text style={styles.lightGray}> {version}</Text>
     </View>
   );
 };
